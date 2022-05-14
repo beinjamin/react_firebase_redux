4d2aeceb2bc795bd322db67a4255b510da5deb4b
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-14 14:44:58 +0100

    Mise en place de firebase

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
-// Your web app's Firebase configuration
-const app = firebaseConfig.initializeApp ( {
+
+
+const app = firebase.initializeApp ( {
   apiKey: "AIzaSyCM71QypNuRg4PPDToxn1ovTQpw_7K4GVU",
   authDomain: "react-firebase-redux-95036.firebaseapp.com",
   projectId: "react-firebase-redux-95036",
@@ -12,4 +11,10 @@ const app = firebaseConfig.initializeApp ( {
   messagingSenderId: "512387560812",
   appId: "1:512387560812:web:a8f63a534b34b0029eef49"
 });
+
+
+export const auth = app.auth();
+
+
+export default app;
 
