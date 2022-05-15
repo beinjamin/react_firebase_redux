950df2fef72dab691f071dcc0d57606cc0831956
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-15 23:00:50 +0100

    Mise en place logique botton

--- src/Components/ConnectModal.js
+++ src/Components/ConnectModal.js
@@ -6,10 +6,19 @@ const ConnectModal = () => {
         <div className="connect-modal">
             <div className="header-btn">
                 <button style={{background: signUp ? "rgb(28,28,28)" 
-                : "rgb(83, 83 ,83)"}}>S'inscrire</button>
+                : "rgb(83, 83 ,83)"}}
+                
+                onClick={()=> setSignUp(true)}
+
+                >S'inscrire</button>
                 <button style={{background: signUp ? "rgb(83,83,83)" 
-                : "rgb(28, 28 ,28)"}}>Se Connecter</button>
+                : "rgb(28, 28 ,28)"}}
+                
+                onClick={()=> setSignUp(false)}
+                
+                >Se Connecter</button>
             </div>
+            {signUp ? <SingnUp/>: <Login/>}
         </div>
     );
 };
