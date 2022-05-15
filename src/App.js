d7f50c874deb9f3ec4b2337c2911f85f5b5bd65c
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-15 08:59:53 +0100

    Creation component login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ConnectModal from './Components/ConnectModal';
 
 const App = () => {
   return (
     <div>
 <div className="app-header">
-  </div>      
+<ConnectModal/>
+  </div> 
+
+    <div className="posts-container"></div>     
     </div>
   );
 };
