4f0e2a4217f11d0842ead33955c3edcacfb81dd1
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-15 09:00:40 +0100

    Creation component s'inscrire

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
