384561d2b07dd96726a6c42ecc194a7f0ae55120
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-17 08:38:11 +0100

    Mise en place du formaulaire d'inscription

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>SingUp</h1>
+        <div className="signup-container">
+            <div className="signup">
+                <h3>S'inscrire</h3>
+                <form>
+                    <input type="text" placeholder="Pseudo" required/>
+                    <input type="email" placeholder="Email" required/>
+                    <input type="password" placeholder="Mot de passe" required/>
+                    <input type="submit" placeholder="Valider Inscription" required/>
+
+
+
+                </form>
+
+            </div>
         </div>
     );
 };
