262c48fc912252076191ca199cb5f4d47b4f478f
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-17 08:40:33 +0100

    Gestion des const pour les element du formulaire

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 const SignUp = () => {
+const registerEmail = userRef();
+const registerPassword = userRef();
+
+
+
+
+
+
+
+
     return (
         <div className="signup-container">
             <div className="signup">
                 <h3>S'inscrire</h3>
                 <form>
                     <input type="text" placeholder="Pseudo" required/>
-                    <input type="email" placeholder="Email" required/>
-                    <input type="password" placeholder="Mot de passe" required/>
+                    <input type="email" placeholder="Email" required ref={registerEmail}/>
+                    <input type="password" placeholder="Mot de passe" require ref={registerPassword}/>
                     <input type="submit" placeholder="Valider Inscription" required/>
 
 
