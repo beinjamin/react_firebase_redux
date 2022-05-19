393c349bba5d0d1b7cdd6698fe5398fd0cd97ea0
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-19 19:07:51 +0100

    Update mise en place du systeme deconnexion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from "react";
 import ConnectModal from "./Components/ConnectModal";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import CreatePost from "./Components/CreatePost";
 import { auth } from "./utils/firebase.config";
 
 const App = () => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   //et methode vas surveille si on est connect ou pas
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
   });
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
 
   return (
     <div>
       <div className="app-header">
+        {user && (
+          <div className="user-infos">
+            <span>{user?.displayName[0]}</span>
+            <h4>[user?.displayName]</h4>
+            <button onClick={() => handleLogout}>
+              <i className="fa-solid fa-arrow-right-from-bracket"></i>
+            </button>
+          </div>
+        )}
         {user ? <CreatePost /> : <ConnectModal />}
       </div>
 
