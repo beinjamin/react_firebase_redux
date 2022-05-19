66eed2b5a6e226826661cae234b2abe635957be3
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-19 18:39:47 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React from "react";
 import ConnectModal from "./Components/ConnectModal";
 import { onAuthStateChanged } from "firebase/auth";
 import CreatePost from "./Components/CreatePost";
+import { auth } from "./utils/firebase.config";
 
 const App = () => {
-  const [use, setUser] = useState(null);
+  const [user, setUser] = useState();
   //et methode vas surveille si on est connect ou pas
-  onAuthStateChanged(auth,(currentUser)=>{
+  onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
   });
 
   return (
     <div>
       <div className="app-header">
-        {user ?(
-          <CreatePost/>
-
-        ):(
-          <ConnectModal/>
-        )
-      }
-       
+        {user ? <CreatePost /> : <ConnectModal />}
       </div>
 
       <div className="posts-container"></div>
